d3d539e884c2aa8b0b7ad789ab965061ad58c30a
Rup medhi <rupmmedhi@gmail.com>
2024-02-22 11:25:51 +0530

    creating modal & Schema

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
-
+const Chat = require("./modals/Chat")
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine','ejs')
@@ -15,12 +15,22 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
+let chat1 = new Chat({
+    from : 'neha',
+    to:'priya',
+    msg : ' send me your exam sheet',
+    created_at : new Date( )
+})
+
+chat1.save().then((res)=>{
+    console.log(res);
+})
 app.get('/',(req,res)=>{
     res.send("root is working")
 })
 
 
-app.listen(3000,()=>{
+app.listen(4000,()=>{
     console.log('server is listening port 3000');
 })
 
