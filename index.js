ea877377694d591caff64b2193a6c69af9a7f8b0
Rup medhi <rupmmedhi@gmail.com>
2024-02-23 09:42:20 +0530

    create & edit & delete route

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
 const Chat = require("./modals/Chat")
+const methodOverride = require("method-override")
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine','ejs')
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'));
 
 main()
 .then(()=>console.log('connection successful'))
@@ -24,7 +27,7 @@ let chat1 = new Chat({
 })
 
 chat1.save().then((res)=>{
-    console.log(res);
+    //console.log(res);
 })
 
 // index route
@@ -39,9 +42,64 @@ app.get('/chats/new',(req,res)=>{
     res.render('new.ejs')
 })
 
+//create route
+app.post('/chats',(req,res)=>{
+    let {from,to,msg} = req.body
+    let newChat = new Chat({
+        from : from,
+        to : to,
+        msg :msg,
+        created_at:new Date()
+    })
+  //  console.log(newChat);
+  newChat.save()
+    .then((res)=> console.log('chat was saved'))
+    .catch((err)=>console.log(err))
+
+res.redirect('/chats')
+
+})
+
+
+// edit route
+app.get('/chats/:id/edit',async (req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id)
+    res.render('edit.ejs',{chat})
+})
+
+
+//UPDATE ROUTE
+
+app.put('/chats/:id',async(req,res)=>{
+    let {id} = req.params;
+    let {msg :newMsg} = req.body
+    let updatedChat = await Chat.findByIdAndUpdate(id,{msg:newMsg}, {runValidators:true, new:true} )
+    res.redirect('/chats')
+})
+
+
+//delete route
+app.delete('/chats/:id',async (req,res)=>{
+    let {id} = req.params
+    let deletedChat = await Chat.findByIdAndDelete(id)
+    console.log(deletedChat);
+    res.redirect('/chats')
+})
+
+
 app.get('/',(req,res)=>{
     res.send("root is working")
+     
 })
+
+
+
+
+
+
+
+
 
 
 app.listen(4000,()=>{
